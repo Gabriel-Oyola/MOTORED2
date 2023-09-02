1b77f82d523bc6976f13d48f47fa2b645a94c56c
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-09-02 16:43:57 -0300

    Se creo Models/TallerTop

---
+++ MOTORED2/Shared/Models/TallerTop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOTORED2.Shared.Models
+{
+    public class TallerTop
+    {
+        public int idRanking { get; set; }
+        public int IdTaller { get; set; }
+
+        public int rating { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string LinkTaller { get; set; }
+
+    }
+}
