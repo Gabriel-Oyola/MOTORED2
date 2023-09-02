054450a541489f470e2a099685b6797aba6bafa8
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-09-02 16:48:47 -0300

    Se creo Opiniones

---
+++ MOTORED2/Shared/Models/Opiniones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOTORED2.Shared.Models
+{
+    public class Opiniones
+    {
+        public int IdOpinion { get; set; }
+        public int IdTaller { get; set; }
+
+        public int IdUsuario { get; set; }
+
+        public string Comentario { get; set; }
+
+
+        public DateTime Fecha { get; set; }
+
+        public int Estrellas { get; set; }
+    }
+}
