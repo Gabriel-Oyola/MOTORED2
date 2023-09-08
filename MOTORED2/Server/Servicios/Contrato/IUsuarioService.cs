9e4ac8dbf3bff0845934fc339ef485f8c4fee551
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-09-08 03:22:25 -0300

    Se crean carpetas Servicios/Contrato/Con la interfaz "IUsuarioService.cs"

---
+++ MOTORED2/Server/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MOTORED2.Server.Models; 
+
+
+
+namespace MOTORED2.Server.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+
+        //Aqui declararemos los metodos que vamos utilizar 
+
+        Task<Usuario> GetUsuario(string correo, string clave); //Devolveremos un usuario
+
+        Task<Usuario> SaveUsuario(Usuario Modelo); //Guardaremos un Usuario
+
+    }
+}
