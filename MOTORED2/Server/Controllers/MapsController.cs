965927c55ec0cddb8dd7efa51ea9c0b9b8776119
Alex Gabriel Oyola <gabriel_oyola@outlook.com>
2023-09-11 18:33:03 -0300

    Se creo controlador maps con su vista mapa

---
+++ MOTORED2/Server/Controllers/MapsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOTORED2.Server.Controllers
+{
+    public class MapsController : Controller
+    {
+        public IActionResult Mapa()
+        {
+            return View();
+        }
+    }
+}
